16d5acc9dce1a3028dc0cf9f4e6e71ec7d1bca03
Tolegen Timur <demigodkz@gmail.com>
2022-11-08 16:01:02 +0600

    refactor: change structure for Comments component

--- src/shared/components/Comments/index.tsx
+++ src/shared/components/Comments/index.tsx
@@ -1,6 +1,7 @@
 import { FC, memo } from 'react';
+
 import { IComment } from 'shared/api/lib/types';
-import { isAuth, useUser } from 'shared/common/util';
+import { useUser } from 'shared/common/util';
 import Card from 'shared/components/Card';
 import UserInfo from 'shared/components/UserInfo';
 import AddComment from './ui/AddComment';
@@ -23,15 +24,16 @@ const Comments: FC<IComments> = ({ comments, full, loading }) => {
       <div className="px-3 space-y-2">
         {comments.map((item) => (
           <UserInfo
+            full={full}
             key={item.comment}
             fullName={item.name}
             info={item.comment}
-            avatarUrl={item.avatarUrl || '/noavatar.png'}
+            avatarUrl={item.imageUrl || '/noavatar.png'}
           />
         ))}
         {noComments && <p className="text-sm text-gray-600"> Нет ни одного комментария</p>}
       </div>
-      {full && <AddComment avatarUrl={user?.avatarUrl || ''} />}
+      {full && <AddComment imageUrl={user?.avatarUrl || ''} />}
     </Card>
   );
 };
--- src/shared/components/Comments/ui/AddComment.tsx
+++ src/shared/components/Comments/ui/AddComment.tsx
@@ -1,30 +1,38 @@
-import { FC, memo } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { isAuth } from 'shared/common/util';
+import { FC, memo, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useAddCommentMutation } from 'shared/api';
+import { isAuth, useUser } from 'shared/common/util';
 import Button from 'shared/components/Button';
 
 interface IAddComment {
-  avatarUrl: string;
+  imageUrl: string;
 }
 
-const AddComment: FC<IAddComment> = ({ avatarUrl }) => {
+const AddComment: FC<IAddComment> = ({ imageUrl }) => {
   const navigate = useNavigate();
+  const id = useParams().id as string;
+  const user = useUser();
+  const [comment, setComment] = useState('');
+  const [addComment] = useAddCommentMutation();
 
   const handleClick = () => {
     if (!isAuth()) navigate('/login');
+    addComment({ id, comment, imageUrl, name: user?.fullName });
   };
 
   return (
     <>
       <div className="flex gap-2 p-3">
         <img
-          src={avatarUrl || '/noavatar.png'}
+          src={imageUrl || '/noavatar.png'}
           alt="avatar"
           className="object-cover w-8 h-8 rounded-full"
         />
         <textarea
           id="message"
           rows={3}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-gray-300 focus:ring-0 focus:border-gray-900 resize-none duration-300 transition"
           placeholder="Написать комментарий..."
         />
