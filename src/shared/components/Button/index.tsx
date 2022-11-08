0144ac2fd2b6e4b168bd3bd0aafe7f2e931e3879
Tolegen Timur <demigodkz@gmail.com>
2022-11-08 16:00:44 +0600

    refactor: change structure for Button component

--- src/shared/components/Button/index.tsx
+++ src/shared/components/Button/index.tsx
@@ -1,6 +1,8 @@
 import { ComponentPropsWithoutRef, FC, memo } from 'react';
 import clsx from 'clsx';
 
+import { button, buttonBg, buttonOutline } from './lib/styles';
+
 interface IButton extends ComponentPropsWithoutRef<'button'> {
   outline?: boolean;
 }
@@ -9,15 +11,7 @@ const Button: FC<IButton> = ({ children, className, type, outline = false, ...pr
   <button
     {...props}
     type={type ? 'submit' : 'button'}
-    className={clsx(
-      className,
-      'px-2.5 py-1.5 border-2 text-sm font-medium rounded shadow-xl text-white active:scale-95 transition duration-300 disabled:opacity-50 disabled:scale-100 disabled:pointer-events-none',
-      `${
-        outline
-          ? 'border-gray-900 bg-transparent text-black hover:text-white hover:bg-gray-900 disabled:bg-transparent disabled:text-black'
-          : 'bg-gray-900 border-transparent hover:text-black hover:bg-white hover:border-gray-900 disabled:bg-transparent disabled:text-white disabled:bg-gray-900'
-      }`
-    )}
+    className={clsx(className, button, `${outline ? buttonOutline : buttonBg}`)}
   >
     {children}
   </button>
