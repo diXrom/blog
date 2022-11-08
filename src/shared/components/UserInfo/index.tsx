6b322aa5b2c40c7bda1d6dd92b865f5b451c87e6
Tolegen Timur <demigodkz@gmail.com>
2022-11-08 16:01:56 +0600

    refactor: change structure for UserInfo component

--- src/shared/components/UserInfo/index.tsx
+++ src/shared/components/UserInfo/index.tsx
@@ -1,17 +1,19 @@
 import { FC, memo } from 'react';
+import clsx from 'clsx';
 
 interface IUserInfo {
   fullName: string;
   info: string;
   avatarUrl: string;
+  full?: boolean;
 }
 
-const UserInfo: FC<IUserInfo> = ({ fullName, info, avatarUrl }) => (
+const UserInfo: FC<IUserInfo> = ({ fullName, info, avatarUrl, full }) => (
   <div className="flex items-center gap-2">
     <img src={avatarUrl} alt="avatar" className="object-cover w-8 h-8 rounded-full" />
     <div>
       <div className="text-sm font-semibold text-gray-700">{fullName}</div>
-      <div className="text-xs text-gray-600">{info}</div>
+      <div className={clsx(full ? 'text-md text-gray-800' : 'text-xs text-gray-600')}>{info}</div>
     </div>
   </div>
 );
