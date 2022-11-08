4b89e3850e42c56874c1a1789fe16e9281781adc
Tolegen Timur <demigodkz@gmail.com>
2022-11-08 16:20:05 +0600

    fix: correct img load

--- src/shared/components/UserInfo/index.tsx
+++ src/shared/components/UserInfo/index.tsx
@@ -1,5 +1,7 @@
 import { FC, memo } from 'react';
+import { motion } from 'framer-motion';
 import clsx from 'clsx';
+import { imgAnimation, motionVariants } from 'shared/common/styles';
 
 interface IUserInfo {
   fullName: string;
@@ -10,7 +12,13 @@ interface IUserInfo {
 
 const UserInfo: FC<IUserInfo> = ({ fullName, info, avatarUrl, full }) => (
   <div className="flex items-center gap-2">
-    <img src={avatarUrl} alt="avatar" className="object-cover w-8 h-8 rounded-full" />
+    <motion.img
+      src={avatarUrl}
+      variants={imgAnimation}
+      {...motionVariants}
+      alt=""
+      className="object-cover w-8 h-8 rounded-full"
+    />
     <div>
       <div className="text-sm font-semibold text-gray-700">{fullName}</div>
       <div className={clsx(full ? 'text-md text-gray-800' : 'text-xs text-gray-600')}>{info}</div>
--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -25,7 +25,11 @@ const Header = () => {
     <header className="flex content-center bg-white shadow-md ">
       <div className="container flex justify-between mx-auto px-2.5 md:px-5">
         <Link to="/" className="flex">
-          <img src="/di.svg" alt="di" className="w-24 transition duration-300" />
+          <img
+            src={`${process.env.PUBLIC_URL}/di.svg`}
+            alt="di"
+            className="w-24 transition duration-300"
+          />
         </Link>
         <div className="items-center flex-shrink-0 gap-2.5 flex">
           {pathname === '/' ? isAuth() && NewPost : Home}
